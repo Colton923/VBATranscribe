e35d3d93962a19fe701cb05544929a8b4159a69b
Colton923 <110353948+Colton923@users.noreply.github.com>
2022-10-18 23:31:19 -0400

    Co-authored-by: Lawrence Good <lawrence.good55@gmail.com>

---
+++ blah/Main.ts
@@ -0,0 +1,54 @@
+import type { BuildingData, BuildingInfo, Customer, FramedOpenings, Trim } from './types/Building'
+import inchesInputToImperial from './functions/inchesToImperial'
+
+export class Building {
+	readonly CUSTOMER: Customer
+	readonly DATA: BuildingData
+	readonly UNDERCUT = 4.25
+
+	constructor() {
+		this.CUSTOMER = {} as Customer
+		this.DATA = {
+			info: {},
+			exteriorPanels: {},
+			trim: {},
+			personnelDoors: {},
+			overheadDoors: {},
+			windows: {},
+			framedOpenings: {},
+			insulation: {},
+			additionalOptions: {},
+		} as BuildingData
+	}
+
+	/**
+	 * Converts inches as a string into a number and then converts that number
+	 * into a string with the imperial measurement
+	 *
+	 * @param inches
+	 * @returns {string} string of inches in imperial measurement
+	 */
+	static inchesInputToImperial(inches: string) {
+		return inchesInputToImperial(parseInt(inches))
+	}
+
+	get buildingData() {
+		return this.DATA
+	}
+
+	get buildingInfo() {
+		return this.DATA.info
+	}
+
+	set buildingInfo(buildingInfo: BuildingInfo) {
+		this.DATA.info = buildingInfo
+	}
+
+	set trim(trim: Trim) {
+		this.DATA.trim = trim
+	}
+
+	set framedOpenings(framedOpenings: FramedOpenings) {
+		this.DATA.framedOpenings = framedOpenings
+	}
+}
---
+++ blah/types/buildingComponents.ts
@@ -0,0 +1,9 @@
+import { threeD } from '../walls'
+export type buildingComponents = {
+	FramedOpenings: threeD[]
+	Columns: threeD[]
+    Rafters: threeD[]
+    Purlins: threeD[]
+    Girts: threeD[]
+    Panels: threeD[]
+}
